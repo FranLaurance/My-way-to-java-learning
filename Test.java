8ab98cdc74f1035d3c8c35b65e724ef23de92d1c
Fran <yunpengfan@foxmail.com>
2020-08-23 18:47:32 +0800

    Add files via upload
    first commit

---
+++ Test.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入性别：");
+        String gender = sc.next();
+        System.out.println("请输入成绩：");
+        double score = sc.nextDouble();
+
+        if (score<10){
+            if(gender.equals("男")){
+                System.out.println("进入男子组");
+            }else{
+                System.out.println("进入女子组");
+            }
+
+        }else{
+            System.out.println("淘汰");
+        }
+
+    }
+
+
+}
